93862086b71d6b6e3a57463132538197f749add9
rajikaimal <inbox.rajika@gmail.com>
2016-07-17 23:56:21 +0530

    modularize

--- src/app.js
+++ src/app.js
@@ -1,18 +1,6 @@
 var angular = require('angular');
 
 require('angular-route');
-
-var app = angular.module('ngGHSC', ['ngRoute']);
-
 require('./core/scraper.js');
 
-app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-	$routeProvider.when('/', {
-		templateUrl: 'core/scraper.html',
-		controller: 'scraperCtrl'
-	});
-}]);
-
-app.controller('userCtrl', ['$scope', '$location', function($scope, $location) {
-	alert('done');
-}]);
+var app = angular.module('ngGHSC', ['ngRoute', 'ngGHSC.scraper']);
--- src/core/scraper.js
+++ src/core/scraper.js
@@ -1,7 +1,14 @@
 var angular = require('angular');
 
-var app = angular.module('ngGHSC', []);
+var app = angular.module('ngGHSC.scraper', ['ngRoute']);
+
+app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: 'core/scraper.html',
+		controller: 'scraperCtrl'
+	});
+}]);
 
 app.controller('scraperCtrl', ['$scope', '$location', function($scope, $location) {
-	alert('done');
+	
 }]);
